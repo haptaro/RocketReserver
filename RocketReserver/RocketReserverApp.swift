c5b4c4fd27b9c3262c6d639624e60520e52f7223
Kotaro Fukuo <haptaro.dev@gmail.com>
2021-08-25 09:32:40 -0700

    Initial Commit

---
+++ RocketReserver/RocketReserverApp.swift
@@ -0,0 +1,17 @@
+//
+//  RocketReserverApp.swift
+//  RocketReserver
+//
+//  Created by Kotaro Fukuo on 2021/08/25.
+//
+
+import SwiftUI
+
+@main
+struct RocketReserverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
